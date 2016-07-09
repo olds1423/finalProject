0dea1053fd492e5e0a4a7ea2e671e6989d3e7121
REN BURNETT <renxburnett@gmail.com>
2016-07-09 11:13:06 -0700

    reference to .js files, broke off eventhandler, changed form id in event listener

--- constructor.js
+++ constructor.js
@@ -38,34 +38,3 @@ BillObject.prototype.doAllMethods = function() {
   this.sumTotalRentAndBill();
   this.divideRentEvenly();
 };
-
-//EVENT HANDLING SECTION
-function formSubmitFunction(event) {
-  event.preventDefault();
-
-  //get variables from user
-  var bill = event.target.billName.value;
-  var amount = parseInt(event.target.billAmount.value);
-  var frequency = parseInt(event.target.billFrequency.value);
-  var month = event.target.monthName.value;
-  var rent = parseInt(event.target.totalRent.value);
-  var roomie = event.target.roommateName.value;
-
-  tracker.totalRent = rent;              // total rent
-  tracker.roommateNameArray.push(roomie); //push name to array SHOULD BE SEPARATE INPUT FORM!!!!!
-
-  var newBill = new BillObject(month, amount, frequency, bill); //instantiate new object with user variables
-  newBill.doAllMethods();
-  console.log(newBill);
-
-//clear values
-  event.target.monthName.value = '';
-  event.target.billName.value = '';
-  event.target.billAmount.value = null;
-  event.target.billFrequency.value = null;
-  event.target.totalRent.value = null;
-}
-
-//LISTEN FOR EVENT AND RESPOND
-var form = document.getElementById('dummyForm');   //reference form
-form.addEventListener('submit', formSubmitFunction);
