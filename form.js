6d8a4f0987136634b79212200663ba9a350fa628
Sugarblade <William.f.skelton@outlook.com>
2016-07-20 16:26:23 -0700

    Added njew js files and almost finished restructure.

---
+++ form.js
@@ -0,0 +1,202 @@
+//EVENT HANDLING SECTION
+function newBillEvent(event){
+
+
+  event.preventDefault();
+  var inputBillName = document.getElementById("billName");
+  var inputBillAmount = document.getElementById("billAmount");
+  var currentBillsList = document.getElementById("billList");
+  var currentMonth = document.getElementById("monthName");
+  var billFrequency = document.getElementById("billFrequency");
+  var bill = new BillObject(currentMonth.value, inputBillAmount.value, billFrequency.value, inputBillName.value);
+  bill.findAndUpdateMonth();
+
+
+
+	var billSelector = document.getElementById('selectedBill');
+	var billOption = document.createElement('option');
+	billOption.value = inputBillName.value;
+	billOption.textContent = inputBillName.value + " - " + currentMonth.value;
+	billOption.id = inputBillName.value;
+	billSelector.appendChild(billOption);
+
+
+
+  var newListItem = document.createElement("li");
+  newListItem.textContent = inputBillName.value + " $" + inputBillAmount.value + " - " + currentMonth.value;
+	newListItem.id = inputBillName.value + '-li';
+  currentBillsList.appendChild(newListItem);
+  inputBillName.value = "";
+  inputBillAmount.value = "";
+
+
+
+  helperFunctions.pushToLocalStorage();
+
+
+}
+
+function newRoommateEvent(event){
+  event.preventDefault();
+  var targetMonth = document.getElementById('monthName');
+  var inputRoommate = document.getElementById("newRoomate");
+  var currentRoommateList = document.getElementById("roommateList");
+	var roommateSelector = document.getElementById('selectedRoomate');
+
+
+
+  var child = document.createElement("li");
+	child.textContent = inputRoommate.value + " - " + targetMonth.value;
+	child.id = inputRoommate.value + '-li';
+
+
+
+	var roomateOption = document.createElement('option');
+	roomateOption.value = inputRoommate.value;
+	roomateOption.id = inputRoommate.value
+	roomateOption.textContent = inputRoommate.value + " - " + targetMonth.value;
+
+
+
+
+
+  for (var i = 0; i < allMonths.length; i++) {
+    if(allMonths[i].monthName === targetMonth.value){
+      allMonths[i].roommateNameArray.push(inputRoommate.value);
+
+    }
+
+
+  }
+	roommateSelector.appendChild(roomateOption);
+  currentRoommateList.appendChild(child);
+  inputRoommate.value = "";
+
+  helperFunctions.pushToLocalStorage
+
+
+}
+
+function removeRoomate(event){
+	event.preventDefault();
+
+	var selectedRoomate = document.getElementById('selectedRoomate');
+	var targetMonth = document.getElementById('monthName');
+	var currentRoommateList = document.getElementById("roommateList");
+
+
+
+	for (var i = 0; i < allMonths.length; i++) {
+		var nameLocation = allMonths[i].roommateNameArray.indexOf(selectedRoomate.value);
+		if(targetMonth.value === allMonths[i].monthName){
+			if (nameLocation > -1) {
+				allMonths[i].roommateNameArray.splice(nameLocation, 1);
+				var child = document.getElementById(selectedRoomate.value + '-li')
+				currentRoommateList.removeChild(child);
+
+				child = document.getElementById(selectedRoomate.value);
+				selectedRoomate.removeChild(child);
+
+				break;
+			}
+			else{
+				break;
+			}
+		}
+	}
+
+
+  helperFunctions.pushToLocalStorage();
+}
+
+function removeBill(event) {
+	event.preventDefault();
+
+	var selectedBill = document.getElementById('selectedBill');
+	var targetMonth = document.getElementById('monthName');
+	var currentBillList = document.getElementById("billList");
+
+	for (var i = 0; i < allMonths.length; i++) {
+		var billObjectLocation = allMonths[i].billNameArray.indexOf(selectedBill.value);
+
+		for (var j = 0; j < allMonths[i].billObjectArray.length; j++) {
+
+			if(selectedBill.value === allMonths[i].billNameArray[j]){
+				if (billObjectLocation > -1) {
+
+					allMonths[i].grandTotal -= allMonths[i].billAmountArray[j];
+					allMonths[i].billObjectArray.splice(billObjectLocation, 1);
+					allMonths[i].billNameArray.splice(billObjectLocation, 1);
+					allMonths[i].billAmountArray.splice(billObjectLocation, 1);
+
+
+					var child = document.getElementById(selectedBill.value + '-li')
+					currentBillList.removeChild(child);
+
+					child = document.getElementById(selectedBill.value);
+					selectedBill.removeChild(child);
+					break;
+				}
+			}
+		}
+	}
+
+
+  helperFunctions.pushToLocalStorage();
+}
+
+function refreshGraphs(event, location) {
+	event.preventDefault();
+
+	var targetMonth = document.getElementById('whichGraph');
+	for (var i = 0; i < allMonths.length; i++) {
+		if(targetMonth.value === allMonths[i].monthName){
+
+			var parent = document.getElementById('graphDiv1');
+			var child = document.getElementById('graph1');
+			parent.removeChild(child);
+
+			child = document.createElement('canvas');
+			child.id = 'graph1';
+			parent.appendChild(child);
+			makeBarChart(allMonths[i], 1);
+
+			var parent = document.getElementById('graphDiv2');
+			var child = document.getElementById('graph2');
+			parent.removeChild(child);
+
+			child = document.createElement('canvas');
+			child.id = 'graph2';
+			parent.appendChild(child);
+			makeDoughnutChart(allMonths[i], 2);
+		}
+	}
+
+
+}
+
+startUpCheckStorage();
+
+
+var newBillButton = document.getElementById("nb");
+newBillButton.addEventListener("click", newBillEvent);
+
+
+
+var newRoommateButton = document.getElementById("nr");
+newRoommateButton.addEventListener("click", newRoommateEvent);
+
+
+
+var refreshButton = document.getElementById('refGraphs');
+refreshButton.addEventListener("click", refreshGraphs);
+
+
+
+var removeRoomateButton = document.getElementById('removeRoomateBbutton');
+removeRoomateButton.addEventListener('click', removeRoomate);
+
+
+
+var removeBillButton = document.getElementById('removeBillButton');
+removeBillButton.addEventListener('click', removeBill);
---
+++ librarian.js
@@ -0,0 +1,193 @@
+// Global variables
+var allMonths = [];
+
+// Constructors
+function BillObject(month, amount, frequency, bill) {   //bill constructor
+  this.billMonthName = month || '';
+  this.billAmount = parseInt(amount) || 0;
+  this.billFrequency = parseInt(frequency) || 0;      //frequency inside of month
+  this.billName = bill || '';
+
+
+}
+
+BillObject.prototype.findAndUpdateMonth = function () {
+  for (var i = 0; i < allMonths.length; i++) {
+    if (allMonths[i].monthName === this.billMonthName) {
+      allMonths[i].billObjectArray.push(this);
+      allMonths[i].billNameArray.push(this.billName);
+      allMonths[i].billAmountArray.push(this.billAmount);
+      allMonths[i].grandTotal += this.billAmount;
+
+
+    }
+  }
+};
+
+function Month(name){
+
+  this.monthName = name;
+  this.billObjectArray = [];
+  this.billNameArray = [];
+  this.billAmountArray = [];
+  this.roommateNameArray = [];
+  this.grandTotal = 0;
+  allMonths.push(this);   //push newly instantiated billobject to array in tracker
+}
+
+// Helper Functions
+var helperFunctions = {
+
+  addAllBills: function (object){
+    object.totalRentAndBills += object.billAmount;  //update running total for all bills during each new instantiation
+
+
+  },
+
+  divideRentEvenly: function (object){
+    var divisor = 0;
+    for (each in object.roommateNameArray) {
+      divisor += 1;
+    }
+
+    return this.totalRentAndBills /= divisor;
+
+  },
+
+  pushToLocalStorage: function() {
+
+
+    var tempBillObjArray = [];
+    for (var i = 0; i < allMonths.length; i++) {
+      var billObjArr = allMonths[i].billObjectArray;
+      for (var j = 0; j < allMonths[i].billObjectArray.length; j++) {
+        tempBillObjArray.push(billObjArr[j]);
+      }
+    }
+    var strAllMonthsBillObjects = JSON.stringify(tempBillObjArray);
+    localStorage.setItem('storedBills', strAllMonthsBillObjects);
+
+
+  },
+
+  doAllMethods: function (object){
+    this.addAllBills(object);
+    this.divideRentEvenly(object);
+    return object.totalRentAndBills;
+
+
+  },
+};
+
+function startUpCheckStorage() {
+
+  var newBill = [];
+  if (localStorage.storedBills) {
+    var parsedBills = JSON.parse(localStorage.getItem('storedBills'));
+    for (var i = 0; i < parsedBills.length; i++){
+			var currentBill = new BillObject(parsedBills[i].billMonthName, parsedBills[i].billAmount, parsedBills[i].billFrequency, parsedBills[i].billName);
+			newBill.push(currentBill);
+			// new BillObject(each.billMonthName, each.billAmount, each.billFrequency, each.billName)
+    }
+
+    for (var j = 0; j < newBill.length; j++) {
+			// console.log(newBill);
+			// console.log(item);
+      newBill[j].findAndUpdateMonth();
+    }
+  }
+  if (localStorage.storedBills) {
+
+
+    for (var i = 0; i < allMonths.length; i++) {
+      for (var j = 0; j < allMonths[i].billNameArray.length; j++) {
+        // console.log(allMonths[i]);
+        // console.log(allMonths[i].billObjectArray);
+        var currentObject = allMonths[i].billObjectArray[j];
+				var billSelector = document.getElementById('selectedBill')
+      	var billOption = document.createElement('option');
+				console.log(document.getElementById('selectedBill'));
+				console.log(billOption);
+      	billOption.value = currentObject.billName;
+      	billOption.textContent = currentObject.billName + " - " + currentObject.billMonthName;
+      	billOption.id = currentObject.billName;
+      	billSelector.appendChild(billOption);
+
+
+        var currentBillsList = document.getElementById("billList");
+        var newListItem = document.createElement("li");
+        newListItem.textContent = currentObject.billName + " $" + currentObject.billAmount + " - " + currentObject.billMonthName;
+        newListItem.id = currentObject.billName + '-li';
+        currentBillsList.appendChild(newListItem);
+      }
+    }
+  }
+}
+
+function makeDoughnutChart(object, placement) {
+	
+	var dataArray = [];
+  for (var i = 0; i < object.billObjectArray.length; i++) {
+    dataArray[i] = object.billAmountArray[i] / object.roommateNameArray.length;
+  }
+  var DoughnutData = {
+    labels: object.billNameArray,
+    datasets: [
+      {
+        data: dataArray,
+        backgroundColor: randColor(object.billObjectArray),
+        borderColor: '#000000',
+        borderWidth: 3,
+        hoverBackgroundColor: '#000000',
+        hoverBorderColor: '#ffffff',
+      }
+    ]
+  };
+
+  var canvas = document.getElementById('graph' + placement);
+  var ctx = canvas.getContext('2d');
+
+  var chart = new Chart(ctx, {
+    type: 'doughnut',
+    data: DoughnutData
+  });
+}
+
+function makeBarChart(object, placement) {
+  var BarData = {
+    labels: object.billNameArray,
+    datasets: [
+      {
+        data: object.billAmountArray,
+        backgroundColor: randColor(object.billObjectArray),
+        borderColor: '#000000',
+        borderWidth: 3,
+        hoverBackgroundColor: '#000000',
+        hoverBorderColor: '#ffffff',
+      }
+    ]
+  };
+
+  var canvas = document.getElementById('graph' + placement);
+  var ctx = canvas.getContext('2d');
+
+  var chart = new Chart(ctx, {
+    type: 'bar',
+    data: BarData
+  });
+}
+
+
+// Object Declarations
+var january = new Month('january');
+var february = new Month('february');
+var march = new Month('march');
+var april = new Month('april');
+var may = new Month('may');
+var june = new Month('june');
+var july = new Month('july');
+var august = new Month('august');
+var september = new Month('september');
+var october = new Month('october');
+var november = new Month('november');
+var december = new Month('december');
